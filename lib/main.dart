4d49b7befad45ef27f055ab4fb32ca917f88644f
Dina <lutdina04@yandex.ru>
2024-02-19 19:21:34 +1000

    add download

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:share/share.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -42,7 +49,7 @@ class _URLInputScreenState extends State<URLInputScreen> {
             Icon(
               Icons.qr_code,
               size: 200,
-              color:  Color.fromARGB(255, 144, 0, 32),
+              color: Color.fromARGB(255, 144, 0, 32),
             ),
             Text(
               'CodeCraft',
@@ -72,7 +79,6 @@ class _URLInputScreenState extends State<URLInputScreen> {
               ),
             ),
             SizedBox(height: 100),
-
             ElevatedButton(
               onPressed: () {
                 Navigator.push(
@@ -84,7 +90,8 @@ class _URLInputScreenState extends State<URLInputScreen> {
                 );
               },
               style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Color.fromARGB(255, 144, 0, 32)),
+                backgroundColor:
+                    MaterialStateProperty.all(Color.fromARGB(255, 144, 0, 32)),
                 shape: MaterialStateProperty.all(CircleBorder()),
                 padding: MaterialStateProperty.all(EdgeInsets.all(25)),
               ),
@@ -94,7 +101,6 @@ class _URLInputScreenState extends State<URLInputScreen> {
                 size: 35,
               ),
             )
-
           ],
         ),
       ),
@@ -102,29 +108,75 @@ class _URLInputScreenState extends State<URLInputScreen> {
   }
 }
 
-class QRDisplayScreen extends StatelessWidget {
+class QRDisplayScreen extends StatefulWidget {
   final String url;
 
   QRDisplayScreen(this.url);
 
   @override
+  _QRDisplayScreenState createState() => _QRDisplayScreenState();
+}
+
+class _QRDisplayScreenState extends State<QRDisplayScreen> {
+  GlobalKey globalKey = GlobalKey();
+
+  Future<void> _captureAndSharePng() async {
+    try {
+      RenderRepaintBoundary? boundary = globalKey.currentContext?.findRenderObject() as RenderRepaintBoundary?;
+      if (boundary != null) {
+        ui.Image image = await boundary.toImage(pixelRatio: 3.0);
+        ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+        if (byteData != null) {
+          Uint8List pngBytes = byteData.buffer.asUint8List();
+
+          Directory tempDir = await getTemporaryDirectory();
+          String tempPath = tempDir.path;
+          File file = File('$tempPath/image.png');
+          await file.writeAsBytes(pngBytes);
+
+          await Share.shareFiles(['$tempPath/image.png'], text: 'QR Code');
+        }
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Generated QR Code'),
-      ),
+      appBar: AppBar(),
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            SizedBox(height: 20.0),
+            SizedBox(height: 20),
             Expanded(
               child: Center(
-                child: QrImageView(data: url),
+                child: QrImageView(data: widget.url),
               ),
             ),
+
+            ElevatedButton(
+              onPressed: () {
+                _captureAndSharePng();
+              },
+              style: ButtonStyle(
+                backgroundColor:
+                MaterialStateProperty.all(Color.fromARGB(255, 144, 0, 32)),
+                shape: MaterialStateProperty.all(CircleBorder()),
+                padding: MaterialStateProperty.all(EdgeInsets.all(25)),
+              ),
+              child: Icon(
+                Icons.download,
+                color: Colors.white,
+                size: 35,
+              ),
+            )
+
           ],
         ),
       ),
